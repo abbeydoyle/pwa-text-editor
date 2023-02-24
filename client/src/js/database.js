128aade0d572d3461c875376ee9e5d3c435e50ce
Abigail Doyle <abigaildoyle3@gmail.com>
2023-02-23 19:57:41 -0800

    fix?

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -22,7 +22,10 @@ export const putDb = async (content) => {
   // Open up the desired object store.
   const store = tx.objectStore('jate');
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ jate: content });
+  const request = store.put({ 
+    id: 1, 
+    value: content
+  });
   // Get confirmation of the request.
   const result = await request;
   console.log('🚀 - data saved to the database', result);
@@ -41,12 +44,13 @@ export const getDb = async () => {
   // Open up the desired object store.
   const store = tx.objectStore('jate');
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
   // Error
   console.error('getDb not implemented')
+  return result.value
 };
 
 initdb();
